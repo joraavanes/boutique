f90ea79447d9cadd4a80a5a3344c16287c71cba4
joraavanes <jora_a@outlook.com>
2020-07-17 23:29:19 +0430

    fix bootstrap templating

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
+router.get('/', adminController.products);
+
 // GET: /admin/products
 router.get('/products', adminController.products);
 
